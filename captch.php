798b10c4e520403e171ba5379a5b9f75083f4552
Mahesh Kokkiripati <MAHESH2777@users.noreply.github.com>
2019-07-11 19:28:14 +0530

    changes

---
+++ captch.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <link href="https://fonts.googleapis.com/css?family=Roboto&display=swap" rel="stylesheet">
+    <link rel="shortcut icon" href="images/logom.png" type="image/x-icon">
+    <title>Mugdha Interiors</title>
+    <link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
+    <link href="https://www.fontspace.com/abstrukt?" rel="stylesheet">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+    <link rel="stylesheet" href="style3.css">
+    <script src='https://www.google.com/recaptcha/api.js' async defer></script>
+    </head>
+    <body>
+        <div class="container-fluid recpatch">
+
+<h1>Please Enter The Captcha ...!</h1>
+
+<button><a href="contact.php#cap">Back</button>
+</div>
+
+
+    </body>
+    </html>
